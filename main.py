74bdcedc7ca8dce6a6c29fed05193b3e6238b6d5
ikkefornoyd <ikkefornody777@gmail.com>
2020-08-09 22:30:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import time
 
 cevirilecek = input('Çevirmek istediğiniz Kelime/Cümle: ')
 
+data1= {"detect_new_text":0,
+       "rtk_priv":"none",
+       "rtk_p":"%7B%7D",
+       "_gat_gtag_UA_3411294_31":1,
+       "text_to_translate":cevirilecek}
+
+abc = requests.post("https://www.translate.com/translator/ajax_lang_auto_detect",data=data1).json()
+
+lang = abc["language"]
 
 url = "https://www.translate.com/translator/ajax_translate"
 
