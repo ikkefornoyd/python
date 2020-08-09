fe33682fea10557471b7185b6b8c7f30b5b6c274
ikkefornoyd <ikkefornody777@gmail.com>
2020-08-09 22:30:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import requests
+import time
 
+cevirilecek = input('Çevirmek istediğiniz Kelime/Cümle: ')
 
 
 url = "https://www.translate.com/translator/ajax_translate"
 
-a = requests.post(url,data={"text_to_translate":"Merhaba arkadaşlar benim adım serkan"})
-cek = a.json()
-print(cek["translated_text"])
+if lang == "tr":
+    cevir = requests.post(url,data={"text_to_translate":cevirilecek,"source_lang":lang,"translated_lang":"en"})
+
+else:
+    cevir = requests.post(url,data={"text_to_translate":cevirilecek,"source_lang":lang,"translated_lang":"tr"})
+
+cek = cevir.json()
+
+if cek["result"] == "success":
+    print(cek["translated_text"])
+
+elif cek["result"] == "error":
+    print("Üzgünüm çeviremedim ")
+
+else:
+    pass
