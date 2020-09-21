23c944c1fd1cf65c4df75d7e21c75cba27f7fb9b
ikkefornoyd <ikkefornody777@gmail.com>
2020-09-21 12:56:18 +0300

    Create deneme.py

---
+++ deneme.py
@@ -0,0 +1,27 @@
+# import time
+# a = ["deneme","yapiyorum","arkdaslar","siz kimsiniz","lan","bu bizim kaderimiz"]
+# b = ["ne yapsak","da","faydasiz","kahrolmadan ikimiz","bitmeli bu sevgimiz","dayan yuregim dayan"]
+
+# uz_list = []
+
+# for x in a:
+#     j = len(x)
+#     j = int(j)
+#     uz_list.append(j)
+# e = max(uz_list)
+# print(e)
+# for i in range(len(a)):
+#     deneme = a[i]
+#     toplam_uzunluk = e-len(deneme)
+#     deneme = a[i]+toplam_uzunluk*" "+" |  "+b[i]
+#     print(deneme)
+#     time.sleep(0.05)
+
+
+
+# a = "merhaba arkdasalr benim adim serkan"
+# liste_a = a.split(" ")
+# e = "+".join(liste_a)
+# print(e)
+
+
